ebd50ca2f6db8f12e6388d447d5af0515125e268
Mehmood Hassan <mehmood.mh56@gmail.com>
2026-07-30 13:38:19 +0500

    Time: 7 ms (88.27%), Space: 20.5 MB (78.64%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,19 +1,41 @@
+# class Solution:
+#     def reverseVowels(self, s: str) -> str:
+#         l, r = 0, len(s) - 1
+#         vowels = set("AEIOUaeiou")
+#         s_list = list(s)
+
+
+#         while l < r:
+#             if s_list[l] in vowels and s_list[r] in vowels:
+#                 s_list[l], s_list[r] = s_list[r], s_list[l]
+#                 l += 1
+#                 r -= 1
+#             elif s_list[l] in vowels:
+#                 r -= 1
+#             else:
+#                 l += 1
+#         
+#         return "".join(s_list)
+
+
+# Accourding to AI
+
+
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        l = 0
-        r = len(s) - 1
-        vowels = set("AEIOUaeiou")
         s_list = list(s)
-
-
+        l, r = 0, len(s) - 1
+        vowels = set("AEIOUaeiou")
+        
         while l < r:
-            if s_list[l] in vowels and s_list[r] in vowels:
-                s_list[l], s_list[r] = s_list[r], s_list[l]
+            while l < r and s_list[l] not in vowels:
                 l += 1
+                
+            while l < r and s_list[r] not in vowels:
                 r -= 1
-            elif s_list[l] in vowels:
-                r -= 1
-            else:
-                l += 1
-        s = "".join(s_list)
-        return s
+                
+            s_list[l], s_list[r] = s_list[r], s_list[l]
+            l += 1
+            r -= 1
+            
+        return "".join(s_list)
